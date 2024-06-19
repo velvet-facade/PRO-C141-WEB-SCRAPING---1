36e5e5cef1213b13791f417f8dae0981f5f2d8d9
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2024-06-19 21:25:46 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,40 @@
 from bs4 import BeautifulSoup as bs
+import requests
 import pandas as pd
-import requests as req
 
-link = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
 
-headers = ["name", "distance", "mass", "radius"]
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
 
-webpage = req.get(link, verify=False)
+page = requests.get(bright_stars_url)
+print(page)
 
-soup = bs(webpage.text, "html.parser")
+soup = bs(page.text,'html.parser')
 
-data = []
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+Lum = []
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('bright_stars.csv')
